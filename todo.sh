5601311195c26db14449d12a384d7c88503e36e2
Viorel Stirbu <viorels@gmail.com>
2013-10-14 13:59:42 +0300

    add todo item from command line using API

---
+++ todo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USER=vio
+PASS=asdf123
+TODO_URL=http://vio-todo.herokuapp.com
+
+TITLE=$@
+if [ -z "$TITLE" ]; then
+    echo "Usage: ./todo.sh task name"
+    exit 1
+fi
+
+curl -u $USER:$PASS -H "Content-Type: application/json" -X POST --data "{\"title\": \"$TITLE\"}" $TODO_URL/api/v1/todo
+echo
+
--- todoapp/models.py
+++ todoapp/models.py
@@ -6,7 +6,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     priority = models.IntegerField(default=1)
-    due = models.DateField()
+    due = models.DateField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
 
     def __unicode__(self):  # Python 3: def __str__(self):
